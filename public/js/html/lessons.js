ae86c2d6abebdc4435e8923f3a7baf62d9575446
yassinhasan <figo781@gmail.com>
2023-08-30 19:45:21 +0300

    add try edit

--- public/js/html/lessons.js
+++ public/js/html/lessons.js
@@ -3,7 +3,7 @@
 
   function checkActive() {
     document.querySelectorAll("nav li").forEach((item) => {
-      console.log(item);
+      // console.log(item);
       if (item.dataset.active == "true") {
         selectedLink.style.transform = `translateX(${
           item.offsetLeft + item.offsetWidth / 2 - 24 + "px"
@@ -13,7 +13,7 @@
   }
   
   document.querySelectorAll("nav li").forEach((item) => {
-    console.log(item.offsetLeft);
+    // console.log(item.offsetLeft);
     item.addEventListener("click", (e) => {
       document.querySelectorAll("nav li").forEach((i) => {
         i.dataset.active = "false";
@@ -26,3 +26,15 @@
   checkActive();
   
   // end navbar
+
+  let html_code_div = document.querySelector(".html_code");
+  let html_preview = document.querySelector(".html_preview");
+  let run = document.querySelector(".run");
+
+  run.addEventListener("click",runCode)
+  function runCode()
+  {
+    html_preview.contentDocument.body.innerHTML= html_code_div.value;
+    console.log(html_preview);
+    console.log( html_code_div.value)
+  }
